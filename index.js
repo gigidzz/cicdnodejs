320366179a0511b25457694340c47a327f4d355e
kiu_dzimistarishvili.gio <dzimistarishvili.gio@kiu.edu.ge>
2025-04-14 22:59:23 +0400

    add express server

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-// index.js
-console.log("Hello from Node.js!");
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from Node.js on Heroku!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
